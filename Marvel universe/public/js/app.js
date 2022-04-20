8c20c50604c7f3812e4f58ad441650c4981e3257
Owen Rubio <owenjavier1004@gmail.com>
2022-04-19 20:25:39 -0500

    responsive navigation bar and slider for main page added

--- Marvel universe/public/js/app.js
+++ Marvel universe/public/js/app.js
@@ -1,2 +1,39 @@
-const links = document.querySelector('.links');
-console.log(links.style)
+//  toggle functionality for <800px width devices
+const toggleButton = document.getElementById('toggle-menu');
+toggleButton.addEventListener('click', () => {
+  const menu = document.getElementById('menu-links');
+  menu.classList.toggle('show');
+});
+
+const slider = document.querySelector('.slider');
+const buttonNext = document.getElementById('right-arrow');
+const buttonPrevious = document.getElementById('left-arrow');
+
+buttonNext.addEventListener('click', goNext);
+buttonPrevious.addEventListener('click', goPrevious);
+
+const myInterval = setInterval(goNext, 3300);
+
+function goNext(){
+  const firstSlide = document.querySelectorAll('.slide')[0];
+  slider.style.transition = 'all 500ms ease-in';
+  slider.style.marginLeft = '-200%';
+  setTimeout(() => {
+    slider.style.transition = 'none';
+    slider.insertAdjacentElement('beforeend', firstSlide);
+    slider.style.marginLeft = '-100%';
+  }, 500);
+}
+
+function goPrevious(){
+  const slideSections = document.querySelectorAll('.slide');
+  const lastSlide = slideSections[slideSections.length-1];
+  slider.style.transition = 'all 500ms ease-in';
+  slider.style.marginLeft = '0';
+  setTimeout(() => {
+    slider.style.transition = 'none';
+    slider.insertAdjacentElement('afterbegin', lastSlide);
+    slider.style.marginLeft = '-100%';
+  }, 500);
+}
+
