cec2ca711f3744dd4c217a4675907ee253b1b926
Owen Rubio <owenjavier1004@gmail.com>
2022-04-28 22:53:01 -0500

    Several features added (about marvel, latest with newsapi, banner, related links and footer). Main page finished.

--- Marvel universe/index.js
+++ Marvel universe/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+
 app.listen(3000, () => {
 	console.log('listening at 3000');
+})
+
+app.get('/v1/news/:date',(request, response) => {
+	const to = new Date(request.params['date']);
+	const from = new Date(to);
+	from.setDate(from.getDate() - 15);
+
+	const formattedFrom = `${from.getFullYear()}-${from.getMonth()+1}-${from.getDate()}`;
+	const formattedTo = `${to.getFullYear()}-${to.getMonth()+1}-${to.getDate()}`;
+
+	axios.get(`https://newsapi.org/v2/everything?q=marvel&from=${formattedFrom}&to=${formattedTo}&sortBy=popularity&apiKey=${here_your_apikey}`)
+		.then(newsResponse => {
+			response.json(newsResponse.data)
+		})
+		.catch(err => {
+			console.log(err);
+		})
 })
